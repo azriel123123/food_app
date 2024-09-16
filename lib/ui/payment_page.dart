07a51c53c10d0846c5c1bc1088d51a46c64fbf19
azriel123123 <mazrielilham4@gmail.com>
2024-09-16 14:06:21 +0700

    update

--- lib/ui/payment_page.dart
+++ lib/ui/payment_page.dart
@@ -25,7 +25,7 @@ class PaymentPage extends StatelessWidget {
               children: <Widget>[
                 Text(
                   'Item Orders',
-                  style: blackFontStyle3,
+                  style: blackFontStyle1,
                 ),
                 SizedBox(
                   height: 12,
@@ -57,8 +57,14 @@ class PaymentPage extends StatelessWidget {
                             overflow: TextOverflow.clip,
                           ),
                         ),
-                        RatingStars(
-                          rate: transaction.food!.rate!,
+                        Text(
+                          NumberFormat.currency(
+                                  symbol: 'IDR ',
+                                  decimalDigits: 0,
+                                  locale: 'id-ID')
+                              .format(
+                            transaction.food!.price,
+                          ),
                         ),
                       ],
                     ),
@@ -70,6 +76,218 @@ class PaymentPage extends StatelessWidget {
                       ),
                     ),
                   ],
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Divider(
+                  thickness: 0.5,
+                  color: Colors.black,
+                  height: 15,
+                ),
+                Text(
+                  'Detail Transaction',
+                  style: blackFontStyle1.copyWith(fontSize: 16),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(transaction.food!.name!),
+                    Spacer(),
+                    Text(
+                      NumberFormat.currency(
+                              symbol: 'IDR ', decimalDigits: 0, locale: 'id_ID')
+                          .format(transaction.food!.price),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Quantity'),
+                    Spacer(),
+                    Text(
+                      '${transaction.quantity.toString()} item(s)',
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Sub Total'),
+                    Spacer(),
+                    Text(
+                      NumberFormat.currency(
+                        symbol: 'IDR ',
+                        decimalDigits: 0,
+                        locale: 'id_ID',
+                      ).format(
+                          transaction.food!.price! * transaction.quantity!),
+                    ),
+                  ],
+                ),
+                Divider(
+                  thickness: 0.5,
+                  color: Colors.black,
+                  height: 15,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Tax 10%'),
+                    Spacer(),
+                    Text(
+                      NumberFormat.currency(
+                        symbol: 'IDR ',
+                        decimalDigits: 0,
+                        locale: 'id_ID',
+                      ).format(transaction.food!.price! *
+                          transaction.quantity! *
+                          0.1),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Driver'),
+                    Spacer(),
+                    Text(
+                      NumberFormat.currency(
+                        symbol: 'IDR ',
+                        decimalDigits: 0,
+                        locale: 'id_ID',
+                      ).format(50000),
+                    ),
+                  ],
+                ),
+                Divider(
+                  thickness: 0.5,
+                  color: Colors.black,
+                  height: 15,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'Total',
+                      style: blackFontStyle2,
+                    ),
+                    Spacer(),
+                    Text(
+                      NumberFormat.currency(
+                        symbol: 'IDR ',
+                        decimalDigits: 0,
+                        locale: 'id_ID',
+                      ).format(
+                        transaction.total!,
+                      ),
+                      style: blackFontStyle2,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: DefaultMargin,
+                ),
+                Divider(
+                  thickness: 0.5,
+                  color: Colors.black,
+                  height: 15,
+                ),
+                Text(
+                  'Deliver To :',
+                  style: blackFontStyle3,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: [
+                    Text('Nama Penerima :'),
+                    Spacer(),
+                    Text(transaction.user!.name!),
+                  ],
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Row(
+                  children: [
+                    Text('Email Penerima :'),
+                    Spacer(),
+                    Text(transaction.user!.email!),
+                  ],
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Row(
+                  children: [
+                    Text('Phone Number :'),
+                    Spacer(),
+                    Text(transaction.user!.phoneNumber!),
+                  ],
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Row(
+                  children: [
+                    Text('Addres :'),
+                    Spacer(),
+                    Text(transaction.user!.address!),
+                  ],
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Row(
+                  children: [
+                    Text('House Number :'),
+                    Spacer(),
+                    Text(transaction.user!.houseNumber!),
+                  ],
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Row(
+                  children: [
+                    Text('City :'),
+                    Spacer(),
+                    Text(transaction.user!.city!),
+                  ],
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 12),
+                  height: 45,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: MainColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        )),
+                    onPressed: () {},
+                    child: Text(
+                      'Order Now',
+                      style: blackFontStyle3.copyWith(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
